4ece87ce418199e48c336ffed0df33c774054985
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-09-05 11:59:21 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  access_key  = "${var.accessKey}"
+  secret_key  = "${var.secretKey}"
+  region      = "us-east-2"
+}
+
+resource "aws_instance" "bindu" {
+  ami = "ami-01eb7a7f8688ed90b" #centos 7.6
+  #ami = "ami-006b2db4ca7e39d7d"  #rhel 7 
+  instance_type = "t2.micro"
+  key_name = "bindu"
+  vpc_security_group_ids = [ "sg-011ef8db4885d7d22" ]
+  
+}
+
+output "bindu_ip" {
+  value = "${aws_instance.bindu.*.public_ip}"
+}
