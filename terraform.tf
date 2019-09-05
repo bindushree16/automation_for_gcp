be50b4e3cc52ed668bdf01fac27242ec3d6fdfdc
bindu <bindushreen@jfrog.com>
2019-09-05 18:25:31 +0530

    adding

--- terraform.tf
+++ terraform.tf
@@ -1,18 +1,28 @@
-provider "aws" {
-  access_key  = "${var.accessKey}"
-  secret_key  = "${var.secretKey}"
-  region      = "us-east-2"
+provider "google" {
+  credentials = "${file("My First Project-4de6cc1605a7.json")}"
+  project     = "${var.project}"
+  region      = "${var.region}"
 }
 
-resource "aws_instance" "bindu" {
-  ami = "ami-01eb7a7f8688ed90b" #centos 7.6
-  #ami = "ami-006b2db4ca7e39d7d"  #rhel 7 
-  instance_type = "t2.micro"
-  key_name = "bindu"
-  vpc_security_group_ids = [ "sg-011ef8db4885d7d22" ]
-  
-}
+resource "google_compute_instance" "default" {
+  name         = "terraform-test"
+  machine_type = "${var.instance_type}"
+  zone         = "${var.zone_id}"
 
-output "bindu_ip" {
-  value = "${aws_instance.bindu.*.public_ip}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image_name}"
+      size = "${var.disk_size}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
 }
--- variable.tf
+++ variable.tf
@@ -1,8 +1,18 @@
-# main creds for AWS connection
-variable "accessKey" {
-  description = "AWS access key"	  
+variable "instance_type" {
+default = "n1-standard-1"
 }
-
-variable "secretKey" {
-  description = "AWS secert access key"
+variable "zone_id" {
+default = "us-central1-a"
+}
+variable "image_name" {
+default = "ubuntu-os-cloud/ubuntu-1604-lts"
+}
+variable "disk_size" {
+default = 30
+}
+variable "region" {
+ default = "us-central1"
+}
+variable "project" {
+ default = "pure-spring-242015"
 }
